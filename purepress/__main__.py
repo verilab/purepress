e88d3768df06ef256a890c63ac8cdf891a6441f5
Richard Chien <stdrc@outlook.com>
2022-03-15 14:52:34 +0800

    use context to reduce redundant code

--- purepress/__main__.py
+++ purepress/__main__.py
@@ -4,6 +4,7 @@ import shutil
 import functools
 import traceback
 from urllib.parse import urlparse
+from contextlib import contextmanager
 
 import click
 from flask import url_for
@@ -25,6 +26,13 @@ echo = click.echo
 echo_green = functools.partial(click.secho, fg="green")
 echo_red = functools.partial(click.secho, fg="red")
 echo_yellow = functools.partial(click.secho, fg="yellow")
+
+
+@contextmanager
+def step(op_name: str):
+    echo(f"{op_name}...", nl=False)
+    yield
+    echo_green("OK")
 
 
 @click.group(name="purepress", short_help="A simple static blog generator.")
@@ -58,24 +66,20 @@ def init_command():
     if os.listdir(root_folder):
         echo_red(f'The instance folder "{root_folder}" is not empty')
         exit(1)
-    echo("Creating folders...", nl=False)
-    os.makedirs(posts_folder, exist_ok=True)
-    os.makedirs(pages_folder, exist_ok=True)
-    os.makedirs(static_folder, exist_ok=True)
-    os.makedirs(raw_folder, exist_ok=True)
-    echo_green("OK")
-    echo("Creating default purepress.toml...", nl=False)
-    with open(os.path.join(root_folder, "purepress.toml"), mode="w", encoding="utf-8") as f:
-        f.write(DEFAULT_PUREPRESS_TOML)
-    echo_green("OK")
-    echo("Createing demo page...", nl=False)
-    with open(os.path.join(pages_folder, "demo.md"), mode="w", encoding="utf-8") as f:
-        f.write(DEFAULT_POST_TEMPLATE.format("page"))
-    echo_green("OK")
-    echo("Createing demo post...", nl=False)
-    with open(os.path.join(posts_folder, "1970-01-01-demo.md"), mode="w", encoding="utf-8") as f:
-        f.write(DEFAULT_POST_TEMPLATE.format("post"))
-    echo_green("OK")
+    with step("Creating folders"):
+        os.makedirs(posts_folder, exist_ok=True)
+        os.makedirs(pages_folder, exist_ok=True)
+        os.makedirs(static_folder, exist_ok=True)
+        os.makedirs(raw_folder, exist_ok=True)
+    with step("Creating default purepress.toml"):
+        with open(os.path.join(root_folder, "purepress.toml"), mode="w", encoding="utf-8") as f:
+            f.write(DEFAULT_PUREPRESS_TOML)
+    with step("Createing demo page"):
+        with open(os.path.join(pages_folder, "demo.md"), mode="w", encoding="utf-8") as f:
+            f.write(DEFAULT_POST_TEMPLATE.format("page"))
+    with step("Createing demo post"):
+        with open(os.path.join(posts_folder, "1970-01-01-demo.md"), mode="w", encoding="utf-8") as f:
+            f.write(DEFAULT_POST_TEMPLATE.format("post"))
     echo_green("OK! Now you can install a theme and preview the site.")
 
 
@@ -93,9 +97,7 @@ def preview_command(host, port, debug):
 @click.option(
     "--url-root",
     prompt="Please enter the url root (used as prefix of generated url)",
-    help="The url root of your site. For example, if you want to access the site "
-    'through "http://example.com/blog/", "http://example.com/blog/" should be '
-    "passed in as the url root.",
+    help='The url root of your site, e.g. "http://example.com/blog/".',
 )
 def build_command(url_root):
     res = urlparse(url_root)
@@ -129,128 +131,116 @@ def build(client):
     build_archive_folder = os.path.join(build_folder, "archive")
     build_index_page_folder = os.path.join(build_folder, "page")
 
-    echo("Creating build folder...", nl=False)
-    if os.path.isdir(build_folder):
-        shutil.rmtree(build_folder)
-    elif os.path.exists(build_folder):
-        os.remove(build_folder)
-    os.mkdir(build_folder)
-    echo_green("OK")
+    with step("Creating build folder"):
+        if os.path.isdir(build_folder):
+            shutil.rmtree(build_folder)
+        elif os.path.exists(build_folder):
+            os.remove(build_folder)
+        os.mkdir(build_folder)
 
-    echo("Copying raw files...", nl=False)
-    copy_folder_content(raw_folder, build_folder)
-    echo_green("OK")
+    with step("Copying raw files"):
+        copy_folder_content(raw_folder, build_folder)
 
-    echo("Copying theme static files...", nl=False)
-    os.makedirs(build_static_theme_folder, exist_ok=True)
-    copy_folder_content(theme_static_folder, build_static_theme_folder)
-    echo_green("OK")
+    with step("Copying theme static files"):
+        os.makedirs(build_static_theme_folder, exist_ok=True)
+        copy_folder_content(theme_static_folder, build_static_theme_folder)
 
-    echo("Copying static files...", nl=False)
-    copy_folder_content(static_folder, build_static_folder)
-    echo_green("OK")
+    with step("Copying static files"):
+        copy_folder_content(static_folder, build_static_folder)
 
-    echo("Building custom pages...", nl=False)
-    for dirname, _, files in os.walk(pages_folder):
-        if os.path.basename(dirname).startswith("."):
-            continue
-        rel_dirname = os.path.relpath(dirname, pages_folder)
-        os.makedirs(os.path.join(build_pages_folder, rel_dirname), exist_ok=True)
-        for file in filter(lambda f: not f.startswith("."), files):
-            rel_path = os.path.join(rel_dirname, file)
-            dst_rel_path = re.sub(r".md$", ".html", rel_path)
-            dst_path = os.path.join(build_pages_folder, dst_rel_path)
-            rel_url = "/".join(os.path.split(dst_rel_path))
-            with app.test_request_context():
-                url = url_for("page", rel_url=rel_url)
-            res = client.get(url)
-            with open(dst_path, "wb") as f:
-                f.write(res.data)
-    echo_green("OK")
+    with step("Building custom pages"):
+        for dirname, _, files in os.walk(pages_folder):
+            if os.path.basename(dirname).startswith("."):
+                continue
+            rel_dirname = os.path.relpath(dirname, pages_folder)
+            os.makedirs(os.path.join(build_pages_folder, rel_dirname), exist_ok=True)
+            for file in filter(lambda f: not f.startswith("."), files):
+                rel_path = os.path.join(rel_dirname, file)
+                dst_rel_path = re.sub(r".md$", ".html", rel_path)
+                dst_path = os.path.join(build_pages_folder, dst_rel_path)
+                rel_url = "/".join(os.path.split(dst_rel_path))
+                with app.test_request_context():
+                    url = url_for("page", rel_url=rel_url)
+                res = client.get(url)
+                with open(dst_path, "wb") as f:
+                    f.write(res.data)
 
     with app.test_request_context():
         posts = load_posts(meta_only=True)
 
-    echo("Building posts...", nl=False)
-    for post in posts:
-        filename = post["filename"]
-        year, month, day, name = os.path.splitext(filename)[0].split("-", maxsplit=3)
-        dst_dirname = os.path.join(build_posts_folder, year, month, day, name)
-        os.makedirs(dst_dirname, exist_ok=True)
-        dst_path = os.path.join(dst_dirname, "index.html")
+    with step("Building posts"):
+        for post in posts:
+            filename = post["filename"]
+            year, month, day, name = os.path.splitext(filename)[0].split("-", maxsplit=3)
+            dst_dirname = os.path.join(build_posts_folder, year, month, day, name)
+            os.makedirs(dst_dirname, exist_ok=True)
+            dst_path = os.path.join(dst_dirname, "index.html")
+            with app.test_request_context():
+                url = url_for("post", year=year, month=month, day=day, name=name)
+            res = client.get(url)
+            with open(dst_path, "wb") as f:
+                f.write(res.data)
+
+    with step("Building categories"):
+        categories = set(functools.reduce(lambda c, p: c + p.get("categories", []), posts, []))
+        for category in categories:
+            category_folder = os.path.join(build_categories_folder, category)
+            os.makedirs(category_folder, exist_ok=True)
+            with app.test_request_context():
+                url = url_for("category", name=category)
+            res = client.get(url)
+            with open(os.path.join(category_folder, "index.html"), "wb") as f:
+                f.write(res.data)
+
+    with step("Building tags"):
+        tags = set(functools.reduce(lambda t, p: t + p.get("tags", []), posts, []))
+        for tag in tags:
+            tag_folder = os.path.join(build_tags_folder, tag)
+            os.makedirs(tag_folder, exist_ok=True)
+            with app.test_request_context():
+                url = url_for("tag", name=tag)
+            res = client.get(url)
+            with open(os.path.join(tag_folder, "index.html"), "wb") as f:
+                f.write(res.data)
+
+    with step("Building archive"):
+        os.makedirs(build_archive_folder, exist_ok=True)
         with app.test_request_context():
-            url = url_for("post", year=year, month=month, day=day, name=name)
+            url = url_for("archive")
         res = client.get(url)
-        with open(dst_path, "wb") as f:
+        with open(os.path.join(build_archive_folder, "index.html"), "wb") as f:
             f.write(res.data)
-    echo_green("OK")
 
-    echo("Building categories...", nl=False)
-    categories = set(functools.reduce(lambda c, p: c + p.get("categories", []), posts, []))
-    for category in categories:
-        category_folder = os.path.join(build_categories_folder, category)
-        os.makedirs(category_folder, exist_ok=True)
+    with step("Building index"):
         with app.test_request_context():
-            url = url_for("category", name=category)
+            url = url_for("index")
         res = client.get(url)
-        with open(os.path.join(category_folder, "index.html"), "wb") as f:
+        with open(os.path.join(build_folder, "index.html"), "wb") as f:
             f.write(res.data)
-    echo_green("OK")
+        page_num = 2
+        while res.status_code != 404:
+            page_folder = os.path.join(build_index_page_folder, str(page_num))
+            os.makedirs(page_folder, exist_ok=True)
+            with app.test_request_context():
+                url = url_for("index_page", page_num=page_num)
+            res = client.get(url)
+            with open(os.path.join(page_folder, "index.html"), "wb") as f:
+                f.write(res.data)
+            page_num += 1
 
-    echo("Building tags...", nl=False)
-    tags = set(functools.reduce(lambda t, p: t + p.get("tags", []), posts, []))
-    for tag in tags:
-        tag_folder = os.path.join(build_tags_folder, tag)
-        os.makedirs(tag_folder, exist_ok=True)
+    with step("Building feed"):
         with app.test_request_context():
-            url = url_for("tag", name=tag)
+            url = url_for("feed")
         res = client.get(url)
-        with open(os.path.join(tag_folder, "index.html"), "wb") as f:
+        with open(os.path.join(build_folder, "feed.xml"), "wb") as f:
             f.write(res.data)
-    echo_green("OK")
 
-    echo("Building archive...", nl=False)
-    os.makedirs(build_archive_folder, exist_ok=True)
-    with app.test_request_context():
-        url = url_for("archive")
-    res = client.get(url)
-    with open(os.path.join(build_archive_folder, "index.html"), "wb") as f:
-        f.write(res.data)
-    echo_green("OK")
-
-    echo("Building index...", nl=False)
-    with app.test_request_context():
-        url = url_for("index")
-    res = client.get(url)
-    with open(os.path.join(build_folder, "index.html"), "wb") as f:
-        f.write(res.data)
-    page_num = 2
-    while res.status_code != 404:
-        page_folder = os.path.join(build_index_page_folder, str(page_num))
-        os.makedirs(page_folder, exist_ok=True)
+    with step("Building 404"):
         with app.test_request_context():
-            url = url_for("index_page", page_num=page_num)
+            url = url_for("page_not_found")
         res = client.get(url)
-        with open(os.path.join(page_folder, "index.html"), "wb") as f:
+        with open(os.path.join(build_folder, "404.html"), "wb") as f:
             f.write(res.data)
-        page_num += 1
-    echo_green("OK")
-
-    echo("Building feed...", nl=False)
-    with app.test_request_context():
-        url = url_for("feed")
-    res = client.get(url)
-    with open(os.path.join(build_folder, "feed.xml"), "wb") as f:
-        f.write(res.data)
-    echo_green("OK")
-
-    echo("Building 404...", nl=False)
-    with app.test_request_context():
-        url = url_for("page_not_found")
-    res = client.get(url)
-    with open(os.path.join(build_folder, "404.html"), "wb") as f:
-        f.write(res.data)
-    echo_green("OK")
 
 
 def copy_folder_content(src, dst):
